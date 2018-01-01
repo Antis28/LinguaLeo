a7241062aeb232bfb349435587b6bb1b125cbab5
antis <antis28@mail.ru>
2018-01-01 13:34:29 +0300

    ref(word-translate) WordMeneger

--- Assets/LinguaLeo/Scripts/Behaviour/WordManeger.cs
+++ Assets/LinguaLeo/Scripts/Behaviour/WordManeger.cs
@@ -13,7 +13,7 @@ public class WordManeger : MonoBehaviour
 {
 
     public Text wordText;
-    public ButtonComponent[] buttonText;
+    public ButtonComponent[] buttons;
 
     public string folder = "Base"; // подпапка в Resources, для чтения
 
@@ -32,7 +32,7 @@ public class WordManeger : MonoBehaviour
 
     void Awake()
     {
-        buttonText = FindObjectsOfType<ButtonComponent>();
+        buttons = FindObjectsOfType<ButtonComponent>();
     }
 
     public void Start()
@@ -40,68 +40,97 @@ public class WordManeger : MonoBehaviour
         Load();
     }
 
-    void SetQuestStatus( Button button, int i, string name ) // событие, для управлением статуса
+    void SetQuestStatus(Button button, int i, string name) // событие, для управлением статуса
     {
         //button.onClick.AddListener(() => QuestStatus(t));
         print("событие, для управлением статуса");
     }
 
-    void SetNextNode( Button button, int i ) // событие, для перенаправления на другой узел диалога
+    void SetNextNode(Button button, int i) // событие, для перенаправления на другой узел диалога
     {
         button.onClick.AddListener(() => BuildTask(i));
         print("событие, для перенаправления на другой узел диалога");
     }
 
-    void SetExitDialogue( Button button ) // событие, для выхода из диалога
+    void SetExitDialogue(Button button) // событие, для выхода из диалога
     {
         //button.onClick.AddListener(() => CloseWindow());
         print("присвоено событие, для выхода из диалога");
-
     }
 
-    void QuestStatus( string s ) // меняем статус слова
+    void Load()
     {
-        print("меняем статус слова");
-    }
-
-    int FindNodeByID( int i )
-    {
-        int j = 0;
-        foreach( var quest in nodes )
+        TextAsset binary = Resources.Load<TextAsset>(folder + "/" + fileName);
+        if (binary == null)
         {
-            if( quest.id == i )
-                return j;
-            j++;
+            Debug.LogWarning("File not found");
+            return;
         }
-
-        return -1;
+        nodes = new List<QuestionLeo>(TASK_COUNT);
+        WordCollection words = LoadWords(binary.text);
+        LoadTasks(words);
+        BuildTask(0);
     }
 
-    void BuildTask( int current )
+    private void LoadTasks(WordCollection words)
+    {
+        for (int i = 0; i < TASK_COUNT; i++)
+        {
+            QuestionLeo question = GeneratorTask(i, words);
+            nodes.Add(question);
+        }
+    }
+
+    private WordCollection LoadWords(string xmlString)
+    {
+        XmlSerializer Serializer = new XmlSerializer(typeof(WordCollection));
+
+        TextReader reader = new StringReader(xmlString);
+        WordCollection result = Serializer.Deserialize(reader) as WordCollection;
+        reader.Close();
+        return result;
+    }
+
+    private QuestionLeo GeneratorTask(int id, WordCollection words)
+    {
+        QuestionLeo questionLeo = new QuestionLeo();
+        questionLeo.id = id;
+
+        questionLeo.answers = words.GetRandomWords(ANSWER_COUNT);
+
+        int indexOfQuestWord = URandom.Range(0, ANSWER_COUNT);
+
+        questionLeo.questWord = questionLeo.answers[indexOfQuestWord];
+        questionLeo.questWord.isTrue = true;
+
+        return questionLeo;
+    }
+
+    private void BuildTask(int current)
     {
         ClearDialogue();
-        if( exit )
+        if (exit)
             return;
 
         int j = FindNodeByID(current);
-        // добавление слова для перевола
-        wordText.text = nodes[j].questWord;
-        if( j < 0 )
+
+        if (j < 0)
         {
             Debug.LogError(this + " в диалоге [" + fileName + ".xml] отсутствует или указан неверно идентификатор узла.");
             return;
         }
 
-        // добавление текста NPC
-        wordText.text = nodes[j].questWord;
         int toNode = j + 1;
-        if( TASK_COUNT == toNode )
+        if (TASK_COUNT == toNode)
         {
             toNode = 0;
             exit = true;
         }
 
-        for( int i = 0; i < nodes[j].answers.Count; i++ )
+        // добавление слова для перевода
+        wordText.text = nodes[j].questWord.engWord;
+        // добавление вариантов для перевода
+        for (int i = 0; i < nodes[j].answers.Count; i++)
         {
             AddToList(toNode, nodes[j].answers[i].ruWord, nodes[j].answers[i].isTrue);
         }
@@ -111,128 +140,42 @@ public class WordManeger : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(this.gameObject);
     }
 
-    void BuildDialogue( int current )
+    private void AddToList(int toNode, string text, bool isTrue)
     {
-        //ClearDialogue();
+        buttons[id].text.text = text;
 
-        //int j = FindNodeByID(current);
-
-        //if( j < 0 )
-        //{
-        //    Debug.LogError(this + " в диалоге [" + fileName + ".xml] отсутствует или указан неверно идентификатор узла.");
-        //    return;
-        //}
-
-        //// добавление текста NPC
-        //wordText.text = node[j].questWord;
-
-        //for( int i = 0; i < node[j].answer.Count; i++ )
-        //{
-
-        //    AddToList(node[j].answer[i].exit, node[j].answer[i].toNode, node[j].answer[i].text, node[j].answer[i].questStatus, node[j].answer[i].questName, true); // текст игрока
-
-        //}
-    }
-
-    void AddToList( int toNode, string text, bool isTrue )
-    {
-        buttonText[id].text.text = text;
-
-        SetNextNode(buttonText[id].button, toNode);
+        SetNextNode(buttons[id].button, toNode);
         id++;
     }
 
-    void Load()
+   
+
+    /// <summary>
+    /// Поиск ноды по ID 
+    /// </summary>
+    /// <param name="i">ID для поиска</param>
+    /// <returns></returns>
+    int FindNodeByID(int i)
     {
-        if( lastName == fileName ) // проверка, чтобы не загружать уже загруженный файл
+        int j = 0;
+        foreach (var quest in nodes)
         {
-            BuildTask(0);
-            return;
+            if (quest.id == i)
+                return j;
+            j++;
         }
 
-        TextAsset binary = Resources.Load<TextAsset>(folder + "/" + fileName);
-        if( binary == null )
-        {
-            Debug.LogWarning("File not found");
-            return;
-        }
-
-        XmlSerializer Serializer = new XmlSerializer(typeof(WordCollection));
-
-        TextReader reader = new StringReader(binary.text);
-        var result = Serializer.Deserialize(reader);
-        reader.Close();
-
-        WordCollection words = result as WordCollection;
-
-        nodes = new List<QuestionLeo>(TASK_COUNT);
-        for( int i = 0; i < TASK_COUNT; i++ )
-        {
-            // Слово для перевода
-            WordLeo word = words.GetWord();
-            QuestionLeo ql = GeneratorTask(i, word, words.GetWords());
-            nodes.Add(ql);
-        }
-
-
-        //GeneratorDialogue1();
-        BuildTask(0);
+        return -1;
     }
 
-    private QuestionLeo GeneratorTask( int id, WordLeo word, List<WordLeo> alternativeWord )
-    {
-        QuestionLeo questionLeo = new QuestionLeo();
-        questionLeo.answers = new List<AnswerLeo>();
-
-        questionLeo.questWord = word.engWord;
-        questionLeo.id = id;
-
-        List<AnswerLeo> sequential = CreateAnswers(alternativeWord);
-        sequential.Add(CreateAnswer(word, true));
-
-        int index;
-        AnswerLeo answerLeo;
-        List<int> lastIndex = new List<int>(ANSWER_COUNT);
-
-        for( int i = 0; i < ANSWER_COUNT; i++ )
-        {
-            int rndValue = -1;
-            do
-            {
-                rndValue = URandom.Range(0, ANSWER_COUNT);
-            } while( lastIndex.Contains(rndValue) );
-            lastIndex.Add(rndValue);
-
-            questionLeo.answers.Add(sequential[rndValue]);
-        }
-
-        return questionLeo;
-    }
-
-    private AnswerLeo CreateAnswer( WordLeo item, bool isTrue = false )
-    {
-        AnswerLeo al = new AnswerLeo();
-        al.ruWord = item.ruWord;
-        al.engWord = item.engWord;
-        al.isTrue = isTrue;
-        return al;
-    }
-
-    private List<AnswerLeo> CreateAnswers( List<WordLeo> items )
-    {
-        List<AnswerLeo> list = new List<AnswerLeo>();
-        foreach( var item in items )
-        {
-            list.Add(CreateAnswer(item));
-        }
-        return list;
-    }
-
+    /// <summary>
+    /// Очистить кнопки от текста
+    /// </summary>
     void ClearDialogue()
     {
         id = 0;
         wordText.text = "";
-        foreach( ButtonComponent b in buttonText )
+        foreach (ButtonComponent b in buttons)
         {
             b.text.text = string.Empty;
             b.rect.sizeDelta = new Vector2(b.rect.sizeDelta.x, 0);
@@ -241,20 +184,20 @@ public class WordManeger : MonoBehaviour
         }
     }
 
-    int GetINT( string text )
+    int GetINT(string text)
     {
         int value;
-        if( int.TryParse(text, out value) )
+        if (int.TryParse(text, out value))
         {
             return value;
         }
         return 0;
     }
 
-    bool GetBOOL( string text )
+    bool GetBOOL(string text)
     {
         bool value;
-        if( bool.TryParse(text, out value) )
+        if (bool.TryParse(text, out value))
         {
             return value;
         }
@@ -267,7 +210,7 @@ class UniqRandom
     readonly int MAX_COUNT;
     List<int> lastIndex;
 
-    UniqRandom( int max )
+    UniqRandom(int max)
     {
         MAX_COUNT = max;
         lastIndex = new List<int>(MAX_COUNT);
@@ -280,7 +223,7 @@ class UniqRandom
         do
         {
             rndValue = URandom.Range(0, MAX_COUNT);
-        } while( lastIndex.Contains(rndValue) );
+        } while (lastIndex.Contains(rndValue));
 
         return rndValue;
     }
