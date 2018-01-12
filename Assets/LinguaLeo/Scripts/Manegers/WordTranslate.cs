3c3d766d27ba1cc7f76b3e71ce4b4533137fcdbf
antis <antis28@mail.ru>
2018-01-12 17:22:31 +0300

    ref(WordTranslate) перемещение каталог Manegers

---
+++ Assets/LinguaLeo/Scripts/Manegers/WordTranslate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordTranslate : MonoBehaviour, Observer
+{
+
+    public Text wordText;
+    public Image wordImage;
+
+    public Slider scoreSlider;
+    public Text scoreText;   
+    
+    private int answersCount;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        GameManager.Notifications.AddListener(this, GAME_EVENTS.BuildTask);
+    }
+
+    void Observer.OnNotify(Component sender, GAME_EVENTS notificationName)
+    {
+        switch (notificationName)
+        {
+            case GAME_EVENTS.BuildTask:
+                ProgeressUpdate();
+                break;
+        }
+    }
+
+    private void ProgeressUpdate()
+    {
+        answersCount++;
+        scoreText.text = answersCount + "/" + scoreSlider.maxValue;
+        scoreSlider.value = answersCount;
+    }
+}
