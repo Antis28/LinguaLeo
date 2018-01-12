9d3d0adf839cf929535bb1a992a5f9f7e310992b
antis <antis28@mail.ru>
2018-01-12 17:20:59 +0300

    ref(LevelManeger) перемещение каталог parts

---
+++ Assets/LinguaLeo/Scripts/Manegers/Parts/LevelManeger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManeger : MonoBehaviour, Observer
+{
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void QuitGame(string name)
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        //Application.loadedLevel
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void Observer.OnNotify(Component sender, GAME_EVENTS notificationName)
+    {
+        switch (notificationName)
+        {
+            case GAME_EVENTS.Exit:
+                LoadLevel("result");
+                break;
+        }
+
+    }
+
+    public void Start()
+    {
+        GameManager.Notifications.AddListener(this, GAME_EVENTS.Exit);
+    }
+}
