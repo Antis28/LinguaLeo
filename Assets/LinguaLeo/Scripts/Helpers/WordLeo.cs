4ec17e1af1f1710cc2cc665237f122b77fcacc16
antis <antis28@mail.ru>
2018-01-19 15:41:40 +0300

    add(WordLeo) обновил класс для сериализации
    согласно экспортеру

--- Assets/LinguaLeo/Scripts/Helpers/WordLeo.cs
+++ Assets/LinguaLeo/Scripts/Helpers/WordLeo.cs
@@ -2,25 +2,37 @@
 
 /// <summary>
 /// Mapping Class
+/// см. класс ExportWordLeo
+/// в проекте Обучение/CSVReader
 /// </summary>
 public class WordLeo
 {
     [XmlAttribute]
     public string wordValue;
+
     [XmlAttribute]
     public string translations;
+
     [XmlAttribute]
     public string pictureURL;
+
     [XmlAttribute]
     public string transcription;
+
     [XmlAttribute]
     public string highlightedContext;
+
     [XmlAttribute]
     public string soundURL;
-    [XmlAttribute]
-    public string groups;
+
+    [XmlArray]
+    [XmlArrayItem("group")]
+    public string[] groups;
+
     [XmlAttribute]
     public string clozefiedContext;
+
+    public WordLeo() { }
 
     override
     public string ToString()
