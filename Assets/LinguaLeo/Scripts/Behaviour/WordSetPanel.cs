814d86349787c1a919aad748b82a039b66afadf2
antis <antis28@mail.ru>
2018-01-27 02:24:07 +0300

    feature(groupСhoice) GUI для наборов слов
    загрузка наборов слов плитками
    вычисляется высота для соотношения сторон 9:16

---
+++ Assets/LinguaLeo/Scripts/Behaviour/WordSetPanel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordSetPanel : MonoBehaviour
+{
+    [SerializeField]
+    Image LogoImage = null;
+    [SerializeField]
+    Text CaptionText = null;
+    [SerializeField]
+    Text WordCountText = null;
+
+    public void Init(Sprite sprite, string caption, int count)
+    {
+        LogoImage.sprite = sprite;
+        CaptionText.text = caption;
+        WordCountText.text = count + " cлов";
+
+        GetComponent<Transform>().localScale = Vector3.one;
+        GetComponent<Transform>().localPosition = Vector3.zero;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Test()
+    {
+        Debug.Log("Test");
+    }
+
+    // OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider
+    public void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+    }
+
+    // OnMouseUp is called when the user has released the mouse button
+    public void OnMouseUp()
+    {
+        Debug.Log("OnMouseUp");
+    }
+
+    // OnMouseExit is called when the mouse is not any longer over the GUIElement or Collider
+    public void OnMouseExit()
+    {
+
+    }
+
+    // OnMouseEnter is called when the mouse entered the GUIElement or Collider
+    public void OnMouseEnter()
+    {
+
+    }
+}
