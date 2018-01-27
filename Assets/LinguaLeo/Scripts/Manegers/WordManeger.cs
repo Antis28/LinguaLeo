1f59e140f2fcbd7cdd9f183bf545fe12a8d0b2ec
antis <antis28@mail.ru>
2018-01-27 16:54:16 +0300

    add(WordManeger) доступ к общему словарю

--- Assets/LinguaLeo/Scripts/Manegers/WordManeger.cs
+++ Assets/LinguaLeo/Scripts/Manegers/WordManeger.cs
@@ -19,7 +19,12 @@ public class WordManeger : MonoBehaviour
     [SerializeField]
     private string fileName = string.Empty;
 
-   
+
+    public WordCollection GetVocabulary()
+    {
+        return vocabulary;
+    }
+
     public List<string> GetGroupNames()
     {
         return vocabulary.FilterGroup();
@@ -33,6 +38,7 @@ public class WordManeger : MonoBehaviour
     void Start()
     {
         LoadVocabulary();
+        CreateWordGroups();
     }
 
     private void LoadVocabulary()
@@ -54,7 +60,7 @@ public class WordManeger : MonoBehaviour
 
         GameManager.Notifications.PostNotification(null, GAME_EVENTS.LoadedVocabulary);
     }
-    
+
     private WordCollection LoadFromXml(string xmlString)
     {
         XmlSerializer Serializer = new XmlSerializer(typeof(WordCollection));
@@ -63,5 +69,54 @@ public class WordManeger : MonoBehaviour
         WordCollection result = Serializer.Deserialize(reader) as WordCollection;
         reader.Close();
         return result;
+    }
+
+    public void CreateWordGroups()
+    {
+        List<string> groupNames = GetGroupNames();
+        List<WordGroup> groups = new List<WordGroup>();
+        foreach (string name in groupNames)
+        {
+            LoadGroup(name);
+            int count = GameManager.WordManeger.GetVocabulary().wordsFromGroup.Count;
+            List<WordProgress> wordProgress = new List<WordProgress>();
+            foreach (WordLeo item in GameManager.WordManeger.GetVocabulary().wordsFromGroup)
+            {
+                wordProgress.Add(new WordProgress() { word = item.wordValue });
+            }
+
+            groups.Add(new WordGroup() {
+                name = name,
+                wordCount = count,
+                progress = wordProgress,
+                pictureName = "504"
+            });
+        }
+        SerializeGroup(groups);
+    }
+
+    private List<WordGroup> DeserializeGroup()
+    {
+        string FileName = "WordGroup.xml";
+        FileStream stream = new FileStream(FileName, FileMode.Open);
+
+        XmlSerializer Serializer = new XmlSerializer(typeof(List<WordGroup>));
+
+        TextReader reader = new StreamReader(stream);
+        List<WordGroup> result = Serializer.Deserialize(reader) as List<WordGroup>;
+        reader.Close();
+        Debug.Log("DeserializeGroup");
+        return result;
+    }
+
+    private void SerializeGroup(List<WordGroup> list)
+    {
+        string FileName = "WordGroup.xml";
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<WordGroup>));
+        FileStream Stream = new FileStream(FileName, FileMode.Create);
+        xmlSerializer.Serialize(Stream, list);
+        Stream.Close();
+        Debug.Log("SerializeGroup");
     }
 }
