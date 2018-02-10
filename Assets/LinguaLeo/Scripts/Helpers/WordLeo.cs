a05910ff4aa9ff363344b8efbb769dd33ed096a4
antis <antis28@mail.ru>
2018-02-10 23:36:43 +0300

    feature(Licenses)
    разблокировать тренировки за 1/3 времени
    до истечения времени понижения лицензии

--- Assets/LinguaLeo/Scripts/Helpers/WordLeo.cs
+++ Assets/LinguaLeo/Scripts/Helpers/WordLeo.cs
@@ -46,12 +46,146 @@ public class WordLeo
     /// <param name="word"></param>
     public void ReduceLicense()
     {
-        if (progress.license == WordLicenses.Level_0)
+        if (progress.license == LicenseLevels.Level_0)
             return;
 
-        progress.Reset();
+        progress.ResetWorkouts();
         progress.license--;
         progress.lastRepeat = DateTime.Now;
+    }
+
+    public void ResetLicense()
+    {
+        if (progress.license == LicenseLevels.Level_0)
+        {
+            progress.ResetWorkouts();
+            return;
+        }
+
+        progress.ResetWorkouts();
+        progress.license = LicenseLevels.Level_0;
+        progress.lastRepeat = DateTime.Now;
+    }
+
+
+public void UnlockWorkouts()
+    {
+        progress.ResetWorkouts();
+    }
+
+    public void LicenseValidityCheck()
+    {
+        LicenseLevels license = progress.license;
+        TimeSpan interval = DateTime.Now - progress.lastRepeat;
+        switch (license)
+        {
+            case LicenseLevels.Level_0:
+                progress.ResetWorkouts();
+                break;
+            case LicenseLevels.Level_1:
+                //лицензия на 20 минут
+                if (interval.Minutes > LicenseTimeout.Level_1)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_2:
+                //лицензия на 1 час
+                if (interval.Minutes > LicenseTimeout.Level_2)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_3:
+                //лицензия на  3 часа
+                if (interval.Minutes > LicenseTimeout.Level_3)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_4:
+                //лицензия на 1 сутки
+                if (interval.Minutes > LicenseTimeout.Level_4)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_5:
+                //лицензия на 2 суток
+                if (interval.Minutes > LicenseTimeout.Level_5)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_6:
+                //лицензия на 3 суток
+                if (interval.Minutes > LicenseTimeout.Level_6)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_7:
+                //лицензия на 1 неделю
+                if (interval.Minutes > LicenseTimeout.Level_7)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_8:
+                //лицензия на 1 месяц
+                if (interval.Minutes > LicenseTimeout.Level_8)
+                    ReduceLicense();
+                break;
+            case LicenseLevels.Level_9:
+                //лицензия на 6 месяцев
+                if (interval.Minutes > LicenseTimeout.Level_9)
+                    ReduceLicense();
+                break;
+        }
+    }
+
+    public void CheckingTimeForTraining()
+    {
+        LicenseLevels license = progress.license;
+        TimeSpan interval = DateTime.Now - progress.lastRepeat;
+
+        if (interval.Minutes > LicenseTimeout.Level_1)
+            LicenseValidityCheck();
+
+        switch (license)
+        {
+            case LicenseLevels.Level_1:
+                //лицензия на 20 минут
+                if (interval.Minutes > LicenseTimeTraining.Level_1)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_2:
+                //лицензия на 1 час
+                if (interval.Minutes > LicenseTimeTraining.Level_2)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_3:
+                //лицензия на  3 часа
+                if (interval.Minutes > LicenseTimeTraining.Level_3)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_4:
+                //лицензия на 1 сутки
+                if (interval.Minutes > LicenseTimeTraining.Level_4)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_5:
+                //лицензия на 2 суток
+                if (interval.Minutes > LicenseTimeTraining.Level_5)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_6:
+                //лицензия на 3 суток
+                if (interval.Minutes > LicenseTimeTraining.Level_6)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_7:
+                //лицензия на 1 неделю
+                if (interval.Minutes > LicenseTimeTraining.Level_7)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_8:
+                //лицензия на 1 месяц
+                if (interval.Minutes > LicenseTimeTraining.Level_8)
+                    UnlockWorkouts();
+                break;
+            case LicenseLevels.Level_9:
+                //лицензия на 6 месяцев
+                if (interval.Minutes > LicenseTimeTraining.Level_9)
+                    UnlockWorkouts();
+                break;
+        }
     }
 
     override
