d6da7f8d5631bca549700e19523061e226ad43c2
antis <antis28@mail.ru>
2018-02-12 16:24:00 +0300

    add(LoadSceneButton) переходы между сценами по имени сцены

---
+++ Assets/LinguaLeo/Scripts/Behaviour/LoadSceneButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSceneButton : MonoBehaviour {
+    [SerializeField]
+    private string sceneName = string.Empty;
+
+    private LevelManeger levelManeger;
+
+    // Use this for initialization
+    void Start () {
+        levelManeger = FindObjectOfType<LevelManeger>();
+        GetComponent<Button>().onClick.AddListener(() => levelManeger.LoadLevel(sceneName));
+    }
+}
