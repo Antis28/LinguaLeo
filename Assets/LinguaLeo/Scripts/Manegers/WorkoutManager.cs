446cf73636608f26e68df8b8ece9967c8da7585b
antis <antis28@mail.ru>
2018-03-17 17:20:53 +0300

    feature(WorkoutManager) управление тренировками
    загрузка сцен с тренировками или переходы меду ними
    осуществляетсяся через WorkoutManager, а не напрямую
    через LevelManeger

---
+++ Assets/LinguaLeo/Scripts/Manegers/WorkoutManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkoutManager : MonoBehaviour
+{
+
+    LevelManeger levelManeger;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManeger = FindObjectOfType<LevelManeger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void RunWorkOut(WorkoutNames name)
+    {
+        string sceneName = string.Empty;
+        switch (name)
+        {
+            case WorkoutNames.WordTranslate:
+                sceneName = "worldTranslate";
+                break;
+            case WorkoutNames.TranslateWord:
+                sceneName = "translateWorld";
+                break;
+            case WorkoutNames.Audio:
+                sceneName = string.Empty;
+                break;
+            case WorkoutNames.Puzzle:
+                sceneName = string.Empty;
+                break;
+            case WorkoutNames.reiteration:
+                sceneName = "reiteration";
+                break;
+            case WorkoutNames.brainStorm:
+                sceneName = "brainStorm";
+                break;
+        }
+        if (sceneName != string.Empty)
+            levelManeger.LoadLevel(sceneName);
+    }
+}
