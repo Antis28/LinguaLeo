54c6a84136e48d7d47a4978c2d79b7f723ab4b9a
antis <antis28@mail.ru>
2018-03-17 18:14:05 +0300

    add(WorkoutManager) управление загрузкой сцен с тренировками

--- Assets/LinguaLeo/Scripts/Manegers/WorkoutManager.cs
+++ Assets/LinguaLeo/Scripts/Manegers/WorkoutManager.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorkoutManager : MonoBehaviour
+public class WorkoutManager : MonoBehaviour, Observer
 {
-
     LevelManeger levelManeger;
+    WorkoutNames currentWorkout;
+    List<WordLeo> untrainedWords;
+
+    private int stage;
 
     // Use this for initialization
     void Start()
@@ -20,6 +24,8 @@ public class WorkoutManager : MonoBehaviour
     }
     public void RunWorkOut(WorkoutNames name)
     {
+        currentWorkout = name;
+
         string sceneName = string.Empty;
         switch (name)
         {
@@ -39,10 +45,57 @@ public class WorkoutManager : MonoBehaviour
                 sceneName = "reiteration";
                 break;
             case WorkoutNames.brainStorm:
-                sceneName = "brainStorm";
+                RunBrainStorm();
                 break;
         }
         if (sceneName != string.Empty)
             levelManeger.LoadLevel(sceneName);
+    }
+
+    public List<WordLeo> GetUntrainedWords()
+    {
+        return untrainedWords;
+    }
+
+    private void RunBrainStorm()
+    {
+        switch (stage)
+        {
+            case 0:
+                
+                stage++;
+                break;
+            case 10:
+                GameManager.LevelManeger.LoadWorkOut("result");
+                break;
+                
+        }
+    }
+
+    private void LoadTasks(int QUEST_COUNT)
+    {
+        List<QuestionLeo> questions = new List<QuestionLeo>(QUEST_COUNT);
+        untrainedWords = GameManager.WordManeger.GetUntrainedGroupWords(currentWorkout);
+
+        for (int i = 0; i < QUEST_COUNT; i++)
+        {
+            untrainedWords = ShuffleList(untrainedWords);
+            QuestionLeo question = GeneratorTask(i, questions);
+
+            if (question == null)
+                break;
+            questions.Add(question);
+        }
+    }
+
+    void Observer.OnNotify(Component sender, GAME_EVENTS notificationName)
+    {
+        switch (notificationName)
+        {
+            case GAME_EVENTS.WordsEnded:
+                print("ScoreValue = " + GameManager.ScoreKeeper.ScoreValue);
+                RunBrainStorm();
+                break;
+        }
     }
 }
