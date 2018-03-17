7e074f1ad822f226a2f335c3ba4de2eaafb7fb84
antis <antis28@mail.ru>
2018-03-17 17:23:00 +0300

    feature(SelectWorkoutButton)
    указывает WorkoutManager какую тренировку повесить на кнопку

---
+++ Assets/LinguaLeo/Scripts/Behaviour/SelectWorkoutButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectWorkoutButton : MonoBehaviour
+{    
+    [SerializeField]
+    private WorkoutNames workoutName = WorkoutNames.WordTranslate;
+
+    // Use this for initialization
+    void Start()
+    {
+            GetComponent<Button>().onClick.AddListener(
+                () => GameManager.WorkoutManager.RunWorkOut(workoutName)
+                );
+    }
+}
