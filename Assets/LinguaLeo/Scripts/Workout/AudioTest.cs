f4a1c5404ee9662a6ed414a1730c01b607affc95
antis <antis28@mail.ru>
2018-03-27 18:30:41 +0300

    add(AudioTest) --->
    реализовано:
    показ правильного ответа
    принятие ответа по кнопке <<Enter>>

--- Assets/LinguaLeo/Scripts/Workout/AudioTest.cs
+++ Assets/LinguaLeo/Scripts/Workout/AudioTest.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class AudioTest : MonoBehaviour, Observer, IWorkout
 {
+    private event UnityAction EnterButtonEvent;
+
     [SerializeField]
     private WorkoutNames WorkoutName = WorkoutNames.WordTranslate;
 
     [SerializeField]
     private Text questionText = null; // Поле для вопроса
     [SerializeField]
+    private Image progressImage; // Картинка прогресса
+    [SerializeField]
     private Image wordImage = null; // Картинка ассоциаци со словом
+    [SerializeField]
+    private Button checkButton = null;
 
     InputField AnswerInputField = null;
 
@@ -23,7 +30,7 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
     [SerializeField]
     private int answersCount; // Число протренированых слов
 
-    private Button RepeatWordButton = null;
+    private Button repeatWordButton = null;
     private Workout core;
 
     private Color correctColor = new Color(59 / 255f,
@@ -33,6 +40,9 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
                                                 38 / 255f,
                                                 29 / 255f);
     private bool isAnswerCorrect;
+    private Text mistakeText;
+    private Text translateText;
+    
 
     WorkoutNames IWorkout.WorkoutName
     {
@@ -57,7 +67,6 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
         switch (notificationName)
         {
             case GAME_EVENTS.CoreBuild:
-                
                 core = parametr as Workout;
                 core.buttonsHandler = GameObject.FindObjectOfType<ButtonsHandler>();
                 core.DrawTask += Core_DrawTask;
@@ -66,11 +75,23 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
                 //FindObjectOfType<DebugUI>().FillPanel(questions);
                 break;
             case GAME_EVENTS.ShowResult:
+                CheckAnswer();
+                ShowQuestion();
+                SetupEnterButton(core.RunNextQuestion);
+
                 ShowImage();
                 WordProgressUpdate();
-                core.RunNextQuestion();
+
                 break;
         }
+    }
+
+    private void SetupEnterButton(UnityAction action)
+    {
+        checkButton.onClick.RemoveAllListeners();
+        checkButton.onClick.AddListener(action);
+        EnterButtonEvent = null;
+        EnterButtonEvent += action;
     }
 
     public QuestionLeo GetCurrentQuest()
@@ -78,36 +99,51 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
         return core.GetCurrentQuest();
     }
 
-    public void CheckAnswer()
+    public void CheckAnswerClick()
+    {
+        GameManager.Notifications.PostNotification(null, GAME_EVENTS.ShowResult);
+    }
+    private void CheckAnswer()
     {
         isAnswerCorrect = AnswerInputField.text == core.GetCurrentWord().wordValue;
-        //ShowQuestion();
         questionText.text = core.GetCurrentWord().wordValue;
+        translateText.text = core.GetCurrentWord().translations;
         if (isAnswerCorrect)
         {
             GameManager.Notifications.PostNotification(this, GAME_EVENTS.CorrectAnswer);
             questionText.color = correctColor;
+            mistakeText.text = string.Empty;
         }
-        else
-            questionText.color = wrongColor;
-        GameManager.Notifications.PostNotification(null, GAME_EVENTS.ShowResult);
+        else {
+            mistakeText.text = AnswerInputField.text;
+            mistakeText.color = wrongColor;
+        }
 
     }
 
     // Use this for initialization
     void Start()
     {
-        RepeatWordButton = GameObject.Find("RepeatWordButton").GetComponent<Button>();
+        repeatWordButton = GameObject.Find("RepeatWordButton").GetComponent<Button>();
+        checkButton = GameObject.Find("CheckButton").GetComponent<Button>();
+
         sayToggle = GameObject.Find("AutoSoundToggle").GetComponent<Toggle>();
         scoreSlider = GameObject.Find("ScoreSlider").GetComponent<Slider>();
+
+        progressImage = GameObject.Find("ProgressImage").GetComponent<Image>();
+        wordImage = GameObject.Find("WordImage").GetComponent<Image>();
+
         scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        mistakeText = GameObject.Find("MistakeText").GetComponent<Text>();
+        translateText = GameObject.Find("TranslateText").GetComponent<Text>();
+
         AnswerInputField = GameObject.Find("AnswerInputField").GetComponent<InputField>();
 
         GameManager.Notifications.AddListener(this, GAME_EVENTS.CoreBuild);
         GameManager.Notifications.AddListener(this, GAME_EVENTS.ShowResult);
 
-        if (RepeatWordButton)
-            RepeatWordButton.onClick.AddListener(() => GameManager.AudioPlayer.SayWord());
+        if (repeatWordButton)
+            repeatWordButton.onClick.AddListener(() => GameManager.AudioPlayer.SayWord());
 
         GameManager.Notifications.PostNotification(null, GAME_EVENTS.ButtonHandlerLoaded);
     }
@@ -115,6 +151,11 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            EnterButtonEvent();
+            EventSystem.current.SetSelectedGameObject(AnswerInputField.gameObject);
+        }
 
     }
 
@@ -129,15 +170,17 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
         HideQuestion();
         AnswerInputField.text = string.Empty;
 
+        SetupEnterButton(CheckAnswerClick);
+
         if (sayToggle.isOn)
             GameManager.AudioPlayer.SayWord();
 
         WordProgressUpdate();
-        WorkoutProgeressUpdate();
+        ProgeressBarUpdate();
         GameObject.FindObjectOfType<DebugUI>().FillPanel(core.tasks);
 
-        // выбор окна диалога как активного, чтобы снять выделение с кнопок диалога
-        EventSystem.current.SetSelectedGameObject(this.gameObject);
+        // выбор элемента как активного
+        EventSystem.current.SetSelectedGameObject(AnswerInputField.gameObject);
         GameManager.Notifications.PostNotification(this, GAME_EVENTS.BuildTask);
     }
 
@@ -165,10 +208,10 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
         {
             progress += 0.25f;
         }
-        //progressImage.fillAmount = progress;
+        progressImage.fillAmount = progress;
     }
 
-    private void WorkoutProgeressUpdate()
+    private void ProgeressBarUpdate()
     {
         answersCount++;
         scoreText.text = answersCount + "/" + scoreSlider.maxValue;
@@ -183,7 +226,7 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
             scoreSlider.maxValue = GameManager.WordManeger.CountWordInGroup();
         scoreText.text = answersCount + "/" + scoreSlider.maxValue;
     }
-    
+
     private void HideImage()
     {
         wordImage.enabled = false;
@@ -196,10 +239,16 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
     private void HideQuestion()
     {
         questionText.gameObject.SetActive(false);
+        mistakeText.gameObject.SetActive(false);
+        translateText.gameObject.SetActive(false);
+        AnswerInputField.gameObject.SetActive(true);
     }
     private void ShowQuestion()
     {
         questionText.gameObject.SetActive(true);
+        mistakeText.gameObject.SetActive(true);
+        translateText.gameObject.SetActive(true);
+        AnswerInputField.gameObject.SetActive(false);
     }
     private void SetImage(string fileName)
     {
@@ -211,5 +260,5 @@ public class AudioTest : MonoBehaviour, Observer, IWorkout
         wordImage.preserveAspect = true;
     }
 
-    
+
 }
