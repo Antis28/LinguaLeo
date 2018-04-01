cad9af6f6e7005dbaf9351b3868c0028aa3c1b33
antis <antis28@mail.ru>
2018-04-01 10:28:34 +0300

    feature(DebugUI) отображение загуженых заданий

---
+++ Assets/LinguaLeo/Scripts/DebugUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour
+{
+    [SerializeField]
+    Text[] words = null;
+
+
+    public void FillPanel(List<QuestionLeo> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] != null)
+                words[i].text = list[i].questWord.wordValue;
+        }
+    }
+}
