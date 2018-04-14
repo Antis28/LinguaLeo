25e2967640c506bcf38d4653528dbf1046023163
antis <antis28@mail.ru>
2018-04-14 15:36:21 +0300

    feature(SearchByFirstChar) поиск набора по первым буквам

--- Assets/LinguaLeo/Scripts/Behaviour/SelectGroup.cs
+++ Assets/LinguaLeo/Scripts/Behaviour/SelectGroup.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class SelectGroup : MonoBehaviour, Observer
 {
-    const int PANEL_HEIGHT = 500;
+    public const int PANEL_HEIGHT = 500;
 
     [SerializeField]
     GameObject content = null;
@@ -39,7 +39,7 @@ public class SelectGroup : MonoBehaviour, Observer
     private IEnumerator CreatePanels() //int spriteName
     {
         List<WordGroup> group = GameManager.WordManeger.GetGroupNames();
-        CalulateContentHight(group.Count);
+        UpdateContentHeight(group.Count);
         foreach (var item in group)
         {
             string path = "Data/Covers" + "/" + item.pictureName + ".png";
@@ -58,7 +58,7 @@ public class SelectGroup : MonoBehaviour, Observer
 
     private void CalulateContentHight()
     {
-        //TODO: вычислять колличество строк динамически
+        //TODO: вычислять колличество колонок динамически
         float clumnCount = 3;
         float panelYSpace = content.GetComponent<GridLayoutGroup>().spacing.y * 2;
         float panelCount = content.transform.childCount;
@@ -76,17 +76,42 @@ public class SelectGroup : MonoBehaviour, Observer
     ///  Вычисляет высоту всех панелей в 3 колонки
     /// </summary>
     /// <param name="panelCount">колличество панелей</param>
-    private void CalulateContentHight(float panelCount, float columnCount = 3)
+    private void UpdateContentHeight(float panelCount, float columnCount = 3)
+    {
+        float height = CalulateHightContainer(panelCount, columnCount);
+        SetSizeContent(height);
+    }
+
+    private void SetSizeContent(float height)
+    {
+        Vector2 size = new Vector2();
+        size.y = height;
+        RectTransform rectContent = content.GetComponent<RectTransform>();
+        rectContent.sizeDelta = size;
+        rectContent.localPosition = Vector3.zero;
+    }
+
+    public void SetHeigtContent(float height)
+    {
+        Vector2 size = new Vector2();
+        size.y = height;
+        RectTransform rectContent = content.GetComponent<RectTransform>();
+        rectContent.localPosition = size;
+    }
+
+    /// <summary>
+    /// Расчет высоты контейнера до последней карточки
+    /// </summary>
+    /// <param name="panelCount"></param>
+    /// <param name="columnCount"></param>
+    /// <returns></returns>
+    public float CalulateHightContainer(float panelCount, float columnCount = 3)
     {
         // Растояние между панелями сверху и снизу
         float panelYSpace = content.GetComponent<GridLayoutGroup>().spacing.y * 2;
         float fullHeightPanel = (PANEL_HEIGHT + panelYSpace);
-        // Расчет высоты контейнера карточек
-        Vector2 size = new Vector2();
-        size.y = fullHeightPanel * panelCount / columnCount - fullHeightPanel;
-
-        RectTransform rectContent = content.GetComponent<RectTransform>();
-        rectContent.sizeDelta = size;
-        rectContent.localPosition = Vector3.zero;
+        // Расчет высоты контейнера до последней карточки
+        float height = fullHeightPanel * panelCount / columnCount - fullHeightPanel;
+        return height;
     }
 }
