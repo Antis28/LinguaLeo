d1a512bddbe05c9f1e7901f601f2e9ac8ead19cc
antis <antis28@mail.ru>
2020-01-26 15:59:39 +0300

    fix сохранение группы слов перед выходом fixes #11

--- Assets/LinguaLeo/Scripts/Behaviour/QuitHelper.cs
+++ Assets/LinguaLeo/Scripts/Behaviour/QuitHelper.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class QuitHelper : MonoBehaviour {
 
+    /// <summary>
+    /// Завершает игру.
+    /// </summary>
     public void QuitGame()
     {
-        GameManager.ExitGame();
+        GameManager.Notifications.PostNotification(null, GAME_EVENTS.QuitGame);
+        GameManager.LevelManeger.QuitGame();
+    }
+
+    /// <summary>
+    /// Возращает на сцену 0.
+    /// </summary>
+    public void RestartGame()
+    {
+        //Load first level        
+        SceneManagerAdapt.LoadScene(0);
     }
 }
--- Assets/LinguaLeo/Scripts/Helpers/Settings.cs
+++ Assets/LinguaLeo/Scripts/Helpers/Settings.cs
@@ -12,12 +12,15 @@ public class Settings
     private static readonly string folderXml = @"Data/";
     [XmlIgnore]
     private static readonly string fileNameXml = "Settings.xml";
-
+    [XmlIgnore]
+    private static readonly string path = folderXml + fileNameXml;
     [XmlIgnore]
     private static Settings instance;
 
-    public string lastWordGroup;
-
+    /// <summary>
+    /// Название последней выбранной группы до выхода из игры.
+    /// </summary>
+    public string lastWordGroup = "";
 
     public static Settings Instance
     {
@@ -35,14 +38,9 @@ public class Settings
     private Settings() { }
 
     public static void SaveToXml()
-    {
-        string path = folderXml + fileNameXml;
-        if (!File.Exists(path))
-            throw new Exception("File not founded");
-
+    {        
         using (TextWriter stream = new StreamWriter(path, false, Encoding.UTF8))
         {
-
             //Now save game data
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
 
@@ -52,9 +50,12 @@ public class Settings
     }
     public static void LoadFromXml()
     {
-        string path = folderXml + fileNameXml;
+        if (!File.Exists(path))
+        {
+            SaveToXml();
+        }
         Settings result = null;
-        using (TextReader Stream = new StreamReader(path, Encoding.UTF8))// (path, FileMode.Open, FileAccess.Read))
+        using (TextReader Stream = new StreamReader(path, Encoding.UTF8))
         {
             XmlSerializer Serializer = new XmlSerializer(typeof(Settings));
             result = Serializer.Deserialize(Stream) as Settings;
@@ -63,4 +64,5 @@ public class Settings
 
         instance = result;
     }
+
 }
