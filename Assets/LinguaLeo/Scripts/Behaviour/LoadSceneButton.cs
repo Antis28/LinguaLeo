ec23967e566609f9b93b09bdd52768d04cbe79a6
antis <antis28@mail.ru>
2020-12-02 21:19:22 +0300

    add android UI для меню старта, выбора слов

--- Assets/LinguaLeo/Scripts/Behaviour/LoadSceneButton.cs
+++ Assets/LinguaLeo/Scripts/Behaviour/LoadSceneButton.cs
@@ -9,13 +9,9 @@ namespace LinguaLeo.Scripts.Behaviour
         [SerializeField]
         private string sceneName = string.Empty;
 
-        private LevelManeger levelManeger;
-
         // Use this for initialization
         void Start () {
-            levelManeger = FindObjectOfType<LevelManeger>();
             GetComponent<Button>().onClick.AddListener(() => GameManager.LevelManeger.LoadLevel(sceneName));
-        
         }
     }
 }
