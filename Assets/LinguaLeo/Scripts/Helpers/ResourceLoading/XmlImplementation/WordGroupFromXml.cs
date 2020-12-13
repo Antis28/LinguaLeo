b741c383223e5a1862eeeefea391af0224331c86
antis <antis28@mail.ru>
2020-12-13 13:52:26 +0300

    add загрузка названий групп

---
+++ Assets/LinguaLeo/Scripts/Helpers/ResourceLoading/XmlImplementation/WordGroupFromXml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using LinguaLeo.Scripts.Helpers.Interfaces;
+
+namespace LinguaLeo.Scripts.Helpers.ResourceLoading.XmlImplementation
+{
+    public class WordGroupFromXml: IVocabularyManager<List<WordGroup>>
+    {
+        private readonly string wordGroupFullPath;
+        private readonly XmlSerialization<List<WordGroup>> xmlSerialization;
+
+        private readonly string wordGroupDirectory = "Base";
+        private readonly string wordGroupFile = "WordGroup.xml";
+        
+        public WordGroupFromXml(string path)
+        {
+            xmlSerialization = new XmlSerialization<List<WordGroup>>();
+            wordGroupFullPath = Path.Combine(path, wordGroupDirectory, wordGroupFile);
+        }
+        
+        public List<WordGroup> Load()
+        {
+            return xmlSerialization.Load(wordGroupFullPath);
+        }
+
+        public void Save(List<WordGroup> saveObject)
+        {
+            xmlSerialization.Save(wordGroupFullPath, saveObject);
+        }
+    }
+}
